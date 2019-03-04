16ad71a30712ff30a11383615fb86c6d39fdf64f
denis96z <zinoviev.denis@yandex.ru>
2019-03-04 15:24:17 +0300

    epoll_wait() works

--- main.asm
+++ main.asm
@@ -41,13 +41,17 @@ main:
 @@:
     mov   [_sock_fd], rax
 
-    ;setnb [_sock_fd]
-    ;cmp   rax, -1
-    ;jne   @f
+    setnb [_sock_fd]
+    cmp   rax, -1
+    jne   @f
 
-    ;panic SET_NONBLOCKING_FAIL_LOG_STR, SET_NONBLOCKING_FAIL_LOG_LEN
+    panic SET_NONBLOCKING_FAIL_LOG_STR, SET_NONBLOCKING_FAIL_LOG_LEN
 
 @@:
+    mov eax, EPOLLIN
+    or  eax, EPOLLET
+    mov [_srv_sock_ev.events], eax
+
     bind [_sock_fd], _sock_addr, SOCK_ADDR_SIZE
     test rax, rax
     jz   @f
@@ -97,19 +101,59 @@ fork_loop:
     print r9, r10
     print NEW_LINE_LOG_STR, NEW_LINE_LOG_LEN
 
-accept_loop:
-    accept [_sock_fd]
+    epollcreate EPOLL_SIZE
+    cmp         rax, -1
+    jne         @f
+
+    panic EPOLL_CREATE_FAIL_LOG_STR, EPOLL_CREATE_FAIL_LOG_LEN
+
+@@:
+    mov [_epoll_fd], rax
+
+    mov rax, [_sock_fd]
+    mov [_srv_sock_ev.data], rax
+
+    epollctl [_epoll_fd], EPOLL_CTL_ADD, [_sock_fd], _srv_sock_ev
+    cmp      rax, -1
+    jne      epoll_loop
+
+    panic EPOLL_CTL_FAIL_LOG_STR, EPOLL_CTL_FAIL_LOG_LEN
+
+epoll_loop:
+    epollwait [_epoll_fd], _clnt_sock_evs, EPOLL_SIZE, EPOLL_TIMEOUT
+    cmp       rax, -1
+    jne       @f
+
+    panic EPOLL_WAIT_FAIL_LOG_STR, EPOLL_WAIT_FAIL_LOG_LEN
+
+@@:
+    mov r12, _clnt_sock_evs
+    mov r14, rax
+
+serve_loop:
+    print SERVE_LOOP_LOG_STR, SERVE_LOOP_LOG_LEN
+
+    dec  r14
+    cmp  r14, -1
+    je   epoll_loop
+
+    mov    r13, qword [r12 + 4] ;TODO possible optimization?
+    cmp    r13, [_sock_fd]
+    jne    @f
+
+    accept r13
     cmp    rax, -1
-    mov    [_clnt_fd], rax
     jne    @f
 
     print ACCEPT_FAIL_LOG_STR, ACCEPT_FAIL_LOG_LEN
-    jmp   accept_loop
+
+    jmp serve_loop
 
 @@:
-    write [_clnt_fd], _http_html_headers, __http_html_headers_size
-    close [_clnt_fd]
-    jmp   accept_loop
+    print ACCEPT_OK_LOG_STR, ACCEPT_OK_LOG_LEN
+    close r13
+
+    jmp serve_loop
 
 segment readable writeable
 
@@ -120,10 +164,13 @@ _config_data_size dq ?
 
 _srv_config srv_config_t
 
-SERVER_PORT equ 80
+SERVER_PORT = 80
 
-_sock_fd dq ?
-_clnt_fd dq ?
+EPOLL_SIZE    = 1024
+EPOLL_TIMEOUT = -1
+
+_sock_fd  dq ?
+_epoll_fd dq ?
 
 _sock_addr sockaddr_in_t SERVER_PORT
 SOCK_ADDR_SIZE = $-_sock_addr
@@ -131,10 +178,11 @@ SOCK_ADDR_SIZE = $-_sock_addr
 _worker_pid_buff db WORKER_PID_BUFF_SIZE dup(' ')
 WORKER_PID_BUFF_SIZE = 16
 
-_sock_ev epoll_event_t
+_srv_sock_ev epoll_event_t
 
-;_clnt_events     epoll_event_t DEFAULT_NUM_WORKERS dup(<>) ;TODO
-;_num_clnt_events dq            ?                           ;TODO
+_clnt_sock_evs     dd EPOLL_SIZE dup(?)
+                   dq EPOLL_SIZE dup(?)
+_num_clnt_sock_evs dq ?
 
 include './http/response.asm'
 
@@ -175,5 +223,20 @@ FORK_FAIL_LOG_LEN = $-FORK_FAIL_LOG_STR
 WORKER_START_LOG_STR db 'worker started, pid:'
 WORKER_START_LOG_LEN = $-WORKER_START_LOG_STR
 
+EPOLL_CREATE_FAIL_LOG_STR db 'epoll_create() fail!',0x0A
+EPOLL_CREATE_FAIL_LOG_LEN = $-EPOLL_CREATE_FAIL_LOG_STR
+
+EPOLL_CTL_FAIL_LOG_STR db 'epoll_ctl() fail!',0x0A
+EPOLL_CTL_FAIL_LOG_LEN = $-EPOLL_CTL_FAIL_LOG_STR
+
+EPOLL_WAIT_FAIL_LOG_STR db 'epoll_wait() fail!',0x0A
+EPOLL_WAIT_FAIL_LOG_LEN = $-EPOLL_WAIT_FAIL_LOG_STR
+
+SERVE_LOOP_LOG_STR db 'serve loop!',0x0A
+SERVE_LOOP_LOG_LEN = $-SERVE_LOOP_LOG_STR
+
 ACCEPT_FAIL_LOG_STR db 'accept() fail!',0x0A
 ACCEPT_FAIL_LOG_LEN = $-ACCEPT_FAIL_LOG_STR
+
+ACCEPT_OK_LOG_STR db 'accept() ok!',0x0A
+ACCEPT_OK_LOG_LEN = $-ACCEPT_OK_LOG_STR
